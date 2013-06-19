9971cf367f05bd78f62a8b597b529c86cc228da2
Andrie <apdevries@gmail.com>
2013-06-19 10:07:19 +0100

    Updated license to Apache 2.0, and asserted copyright to Revolution Analytics

--- R/formulaExpand.R
+++ R/formulaExpand.R
@@ -1,20 +1,19 @@
 #
-#  RevoEnhancements/R/compatibility.R by Derek Norton andAndrie de Vries  Copyright (C) 2012-2013
+# RevoEnhancements/R/formulaExpand.R by Derek Norton and Andrie de Vries
 #
-#  This program is free software; you can redistribute it and/or modify
-#  it under the terms of the GNU General Public License as published by
-#  the Free Software Foundation; either version 2 or 3 of the License
-#  (at your option).
-#
-#  This program is distributed in the hope that it will be useful,
-#  but WITHOUT ANY WARRANTY; without even the implied warranty of
-#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#  GNU General Public License for more details.
-#
-#  A copy of the GNU General Public License is available at
-#  http://www.r-project.org/Licenses/
-#
-#
+# Copyright 2013 Revolution Analytics
+#    
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+# 
+#      http://www.apache.org/licenses/LICENSE-2.0
+# 
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, 
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 
 
 #' Function to expand a dot (.) to represent all variables in an XDF (DF)
