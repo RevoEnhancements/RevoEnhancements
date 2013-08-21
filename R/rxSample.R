d70185f798b2307b8ce41d21d562e3f19a884a4c
Andrie <apdevries@gmail.com>
2013-08-21 18:58:30 +0100

    Improved documentation

--- R/rxSample.R
+++ R/rxSample.R
@@ -19,15 +19,16 @@
 #' Function to sample, with or without replacement, from an XDF to a data frame.
 #' 
 #' @param data xdf File
-#' @param size ???
-#' @param replace ???
+#' @param size Number of rows to sample
+#' @param replace If TRUE, samples with replacement.  Passed to \code{\link{sample.int}}
 #' @export
+#' @return A data frame
 #' @family Data mining functions
 #' @examples
 #' library(RevoScaleR)
 #' xdfFile <- file.path(rxGetOption("sampleDataDir"), "CensusWorkers.xdf")
-#' df <- rxSample2Df(xdfFile , size = 10000, replace = FALSE)
-#' df <- rxSample2Df(xdfFile , size = 10000, replace = TRUE)
+#' df <- rxSample2Df(xdfFile, size = 10000, replace = FALSE)
+#' df <- rxSample2Df(xdfFile, size = 10000, replace = TRUE)
 rxSample2Df <- function(data, size, replace = FALSE) {
   extraRows <- 100
   dataInfo <- rxGetInfo(data, getVarInfo = TRUE)
@@ -61,8 +62,6 @@ rxSample2Df <- function(data, size, replace = FALSE) {
     # Trick to pass R CMD check: create and remove variables without binding
     .rxGet <- .rxSet <- .rxStartRow <- .rxChunkNum <- function() {}
     rm(.rxGet, .rxSet, .rxStartRow, .rxChunkNum)
-    zP <- character()
-    rm(zP)
     
     numRows <- length(dataList[[1]])
     rowNum <- seq_len(numRows) + .rxStartRow - 1 
@@ -75,17 +74,18 @@ rxSample2Df <- function(data, size, replace = FALSE) {
   }
   if (replace) {
     mySamp <- sample.int(dataSize, size = size, replace = replace)
-    ret <- rxDataStep(data, transformFunc = createRandomSampleReplace,
-        transformObjects = list(sample = mySamp, ret = list()),
-        returnTransformObjects = TRUE)
+    ret <- rxDataStep(data, 
+                      transformFunc = createRandomSampleReplace,
+                      transformObjects = list(sample = mySamp, ret = list()),
+                      returnTransformObjects = TRUE)
     ret <- do.call(rbind, ret$ret)
   } else {
     newP <- p + extraRows / dataSize
     ret <- head(rxDataStep(data, 
-        transformFunc = createRandomSample,
-        transformVars = oneVar, 
-        transformObjects = list(zP = newP)), 
-      n = size)
+                           transformFunc = createRandomSample,
+                           transformVars = oneVar, 
+                           transformObjects = list(zP = newP)), 
+                n = size)
   }
   return(ret)
 }
